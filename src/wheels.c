202cd1c0b796add9af9aa07c8b66a475af00b0eb
Embla Bygland <emblatbyg@gmail.com>
2018-12-02 17:39:25 +0100

    wheels now uses gyroscope to calculate turn. Added a function gyroTurn(deg) which makes the robot turn desired amount of degrees (positive or negative)

--- src/wheels.c
+++ src/wheels.c
@@ -1,18 +1,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "ev3/ev3.h"
-#include "ev3/ev3_port.h"
-#include "ev3/ev3_tacho.h"
-
-
+#include "ev3.h"
+#include "ev3_port.h"
+#include "ev3_tacho.h"
 #include <unistd.h>
-#define Sleep( msec ) usleep(( msec ) * 1000 );
-
-int initTachos();
-int turnDegRight(int deg);
-int turnRight();
-int turnLeft();
+#include "ev3_sensor.h"
+#include "wheels.h"
+#include "gyroscope.h"
 
 uint8_t rsn;
 uint8_t lsn;
@@ -20,14 +15,19 @@ int right_port = 66;
 int left_port = 67;
 int max_speed;
 
-int main(){
+void sensorInit();
+int get_gyro_value(int val);
+uint8_t gyro_sn;
 
-  //initializing motors
+int main(){
+  //initializing motors and gyroscope
   initTachos();
+  sensorInit();
   //setting the max_speed variable
   get_tacho_max_speed( rsn, &max_speed );
   //running funcitons
-  turnDegRight(180);
+  gyroTurn(90);
+
   ev3_uninit();
   return 0;
 }
@@ -42,9 +42,11 @@ int initTachos(){
     printf("looking for taco\n");
   }
   if (ev3_search_tacho_plugged_in( right_port,0 , &rsn, 0 )){
+    set_tacho_stop_action_inx(rsn, TACHO_BRAKE);
     printf("found motor connected to port: %d\n", right_port);
   }
-  if (ev3ev3_search_tacho_plugged_in(left_port, 0, &lsn, 0 )){
+  if (ev3_search_tacho_plugged_in(left_port, 0, &lsn, 0 )){
+    set_tacho_stop_action_inx(lsn, TACHO_BRAKE);
     printf("found motor connected to port: %d\n",left_port);
   }
 
@@ -53,40 +55,90 @@ int initTachos(){
 }
 
 
-int turnDegRight(int deg){
+void turnDegRight(int deg){
   printf( "run to relative position...\n" );
   set_tacho_speed_sp( rsn, max_speed / 2 );
   set_tacho_ramp_up_sp( rsn, 0 );
   set_tacho_ramp_down_sp( rsn, 0 );
   set_tacho_position_sp( rsn, deg );
-  set_tacho_stop_action(rsn, TACHO_BRAKE);
 
   set_tacho_speed_sp( lsn, max_speed / 2 );
   set_tacho_ramp_up_sp( lsn, 0 );
   set_tacho_ramp_down_sp( lsn, 0 );
   set_tacho_position_sp( lsn, -deg );
-  set_tacho_stop_action(lsn, TACHO_BRAKE);
 
   set_tacho_command_inx( rsn, TACHO_RUN_TO_REL_POS );
   set_tacho_command_inx( lsn, TACHO_RUN_TO_REL_POS );
 
-  return 0;
 }
-/*
-int turnLeft(){
+
+void gyroTurn(int deg){
+  int current_pos;
+  current_pos = get_gyro_value();
+  int end_pos = current_pos+deg;
+  while(current_pos > end_pos +3 || current_pos < end_pos -3){
+    if(end_pos < current_pos){
+      turnRight();
+    }else{
+      turnLeft();
+    }
+    current_pos = get_gyro_value();
+  }
+}
+
+void turnLeft(){
   set_tacho_speed_sp( rsn, max_speed * 2 / 3 );
   set_tacho_speed_sp( lsn, -max_speed * 2 / 3 );
 
-  set_tacho_time_sp( lsn, 50 );
-  set_tacho_ramp_up_sp( lsn, 10 );
-  set_tacho_ramp_down_sp( lsn, 10 );
-  set_tacho_time_sp( rsn, 50 );
-  set_tacho_ramp_up_sp( rsn, 20 );
-  set_tacho_ramp_down_sp( rsn, 20 );
+  set_tacho_time_sp( lsn, 20 );
+  set_tacho_ramp_up_sp( lsn, 5 );
+  set_tacho_ramp_down_sp( lsn, 5 );
+  set_tacho_time_sp( rsn, 20 );
+  set_tacho_ramp_up_sp( rsn, 5 );
+  set_tacho_ramp_down_sp( rsn, 5 );
 
   set_tacho_command_inx( rsn, TACHO_RUN_TIMED );
   set_tacho_command_inx( lsn, TACHO_RUN_TIMED );
+}
+void turnRight(){
+  set_tacho_speed_sp( rsn, -max_speed * 2 / 3 );
+  set_tacho_speed_sp( lsn, max_speed * 2 / 3 );
 
-  return 0;
+  set_tacho_time_sp( lsn, 20 );
+  set_tacho_ramp_up_sp( lsn, 5 );
+  set_tacho_ramp_down_sp( lsn, 5 );
+  set_tacho_time_sp( rsn, 20 );
+  set_tacho_ramp_up_sp( rsn, 5 );
+  set_tacho_ramp_down_sp( rsn, 5 );
+
+  set_tacho_command_inx( rsn, TACHO_RUN_TIMED );
+  set_tacho_command_inx( lsn, TACHO_RUN_TIMED );
 }                                                                                                                                       33,0-1        Bot
-*/
+
+void sensorInit(){
+  int initResult;
+  initResult = ev3_sensor_init();
+  if(initResult == 0){
+   printf("No sensors connected\n");
+ }else if(initResult == -1){
+   printf("Error when initializing sensors\n");
+ }else{
+   printf("Initialized sensors. %d sensors connected\n", initResult );
+ }
+ if (ev3_search_sensor(LEGO_EV3_GYRO, &gyro_sn, 0)){
+   set_sensor_mode(gyro_sn, "GYRO-ANG");
+   printf("Gyro sensor detected, set in angle detection mode.\n");
+ }else {
+   printf("No gyroscope found\n");
+ }
+}
+
+int get_gyro_value(){
+  //uint8_t gyro_sn;
+  int val;
+  if (ev3_search_sensor( LEGO_EV3_GYRO, &gyro_sn, 0 )){
+    get_sensor_value( 0, gyro_sn, &val);
+    printf("gyro value: %d\n", val);
+  }
+  return val;
+}
