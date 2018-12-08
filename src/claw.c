6a7cf2a87f3f5901be6d7eb51426654c90bc3a99
Yohann Jacob Sandvik <yohannjsandvik@gmail.com>
2018-12-08 20:59:53 +0100

    Added code for grab and throw

--- src/claw.c
+++ src/claw.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "ev3.h"
-#include "ev3_port.h"
-#include "ev3_tacho.h"
+#include "ev3/ev3.h"
+#include "ev3/ev3_port.h"
+#include "ev3/ev3_tacho.h"
 #include <unistd.h>
-#include "ev3_sensor.h"
+#include "ev3/ev3_sensor.h"
 #include "claw.h"
+
+#define Sleep( msec ) usleep(( msec ) * 1000 )
 
 uint8_t sn_big;
 uint8_t sn_small;
 
 uint8_t big_motor;
-int big_motor_port = 66;
+int port_big_motor = 66;
 int relative_position_big_motor;
+int max_speed_big_motor;
+
 uint8_t small_motor;
-int small_motor_port = 67;
-int max_speed; // Might not be neccesary
-// get_tacho_max_speed( sn, &max_speed )
-int initTachos();
+int port_small_motor = 67;
+int relative_position_small_motor;
+int max_speed_small_motor; // Might not be neccesary
+
+int claw_Init();
+int claw_Grab();
 
 int main(){
 
     return 0;
 }
 
-int initTachos(){
+int claw_Init()
+{
   if ( ev3_init() == -1 ){
     printf("could not initialize robot\n");
     return ( 1 );
@@ -33,18 +40,51 @@ int initTachos(){
     Sleep( 1000 );
     printf("looking for taco\n");
   }
-  if (ev3_search_tacho_plugged_in( big_motor_port,0 , &big_motor, 0 )){
-    set_tacho_stop_action_inx(big_motor, TACHO_HOLD);
-    set_tacho_ramp_up_sp( big_motor, 5 );
-    set_tacho_ramp_down_sp( big_motor, 5 );
-    printf("found motor connected to port: %d\n", big_motor_port);
+  // init of big motor in the bottom of claw
+  if (ev3_search_tacho_plugged_in( port_big_motor,0 , &big_motor, 0 )){
+    set_tacho_stop_action_inx(big_motor, TACHO_HOLD); // Test other stop action as well for this motor
+    get_tacho_max_speed( big_motor, &max_speed_big_motor );
+    set_tacho_ramp_up_sp( big_motor, max_speed_big_motor / 4 );
+    set_tacho_ramp_down_sp( big_motor, max_speed_big_motor / 4 );
+    printf("found motor connected to port: %d\n", port_big_motor);
   }
-  if (ev3_search_tacho_plugged_in(small_motor_port, 0, &small_motor, 0 )){
+  // init of small motor at the top of claw. 
+  if (ev3_search_tacho_plugged_in(port_small_motor, 0, &small_motor, 0 )){
     set_tacho_stop_action_inx(small_motor, TACHO_HOLD);
-    set_tacho_ramp_up_sp( small_motor, 5 );
-    set_tacho_ramp_down_sp( small_motor, 5 );
-    printf("found motor connected to port: %d\n",small_motor_port);
+    get_tacho_max_speed( small_motor, &max_speed_small_motor );
+    //set_tacho_ramp_up_sp( small_motor, max_speed_small_motor / 4 ); // Ramp up, og ramp down vil være forskjellig for grab og throw
+    //set_tacho_ramp_down_sp( small_motor, max_speed_small_motor / 4 );
+    printf("found motor connected to port: %d\n",port_small_motor);
   }
   printf( "Tacho is now ready \n" );
+  return 0;
+}
+
+// do {
+//   get_tacho_state_flags( sn, &state );
+// } while ( state );
+// printf( "run to relative position...\n" );
+// set_tacho_speed_sp( sn, max_speed / 2 );
+// set_tacho_ramp_up_sp( sn, 0 );
+// set_tacho_ramp_down_sp( sn, 0 );
+// set_tacho_position_sp( sn, 90 );
+// for ( i = 0; i < 8; i++ ) {
+//   set_tacho_command_inx( sn, TACHO_RUN_TO_REL_POS );
+//   Sleep( 500 );
+// }
+
+int claw_Grab()
+{
+  set_tacho_position_sp( big_motor, 90 );
+  set_tacho_position_sp( small_motor, 90 );
+  set_tacho_position_sp( big_motor, -90 );
+  return 0;
+}
+
+int claw_Throw()
+{
+  set_tacho_ramp_up_sp( small_motor, max_speed_small_motor); // Ramp up, og ramp down vil være forskjellig for grab og throw
+  set_tacho_ramp_down_sp( small_motor, max_speed_small_motor / 2 );
+  set_tacho_speed_sp( small_motor, -max_speed_small_motor );
   return 0;
 }
---
+++ src/claw.h
@@ -0,0 +1,8 @@
+#ifndef DETECT_H
+#define DETECT_H
+
+int claw_Init();
+int claw_Grab();
+int claw_Throw();
+
+#endif
