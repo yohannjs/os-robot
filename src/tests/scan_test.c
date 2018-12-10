d549ad2aea380d405fc36cfe69333ed2cf1e9eee
Trym Sneltvedt <trysne@gmail.com>
2018-12-10 12:39:53 +0100

    Adds simple scan test

--- src/tests/scan_test.c
+++ src/tests/scan_test.c
@@ -1,6 +1,26 @@
+#include <unistd.h>
+#include <stdio.h>
 
+#include "../drive.h"
+#include "../adapters/detect.h"
+
+const int SAMPLE_NUM = 100; 
 
 int main()
 {
+    drive_SensorInit();
+    detect_Init();
+
+    FILE* f;
+    f = fopen("samples.txt")
+
+    drive_ScanTurn(180);
+    
+    for (int i = 0; i < SAMPLE_NUM; i++)
+    {
+        fprintf(f, "%d\n", detect_GetDistance());
+        usleep(100 * 1000);
+    }
+    fclose(f);
     return 0;
 }
