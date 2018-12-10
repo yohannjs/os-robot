d793c00a47ba69e36f47bc3e989b52244df56e90
Kaja Gausdal <kajagausdal@gmail.com>
2018-12-10 17:26:37 +0100

    Fixed folder structure, main() now in claw_tests.c

--- src/claw.c
+++ src/claw.c
@@ -27,31 +27,6 @@ FLAGS_T state_small_motor;
 int big_motor_pos;
 int small_motor_pos;
 
-
-int main(){
-  if(claw_Init() == 0)
-  {
-    printf("Initialized claw \n");
-  } else return -1;
-  sleep(1);
-  claw_Throw();
-  sleep(1);
-  /*sleep(5);
-  get_tacho_position(big_motor, &big_motor_pos);
-  printf("Position: %d \n",big_motor_pos); */
-  //sleep(5);
-  claw_Lower();
-  sleep(1);
-  claw_Grab();
-  bool hold = claw_HoldsBall();
-  printf("Hold value is %d \n", hold);
-  sleep(3);
-  claw_Throw();
-  
-  
-  return 0;
-}
-
 // initialization
 int claw_Init()
 {
---
+++ src/tests/claw_test.c
@@ -0,0 +1,23 @@
+int main(){
+  if(claw_Init() == 0)
+  {
+    printf("Initialized claw \n");
+  } else return -1;
+  sleep(1);
+  claw_Throw();
+  sleep(1);
+  /*sleep(5);
+  get_tacho_position(big_motor, &big_motor_pos);
+  printf("Position: %d \n",big_motor_pos); */
+  //sleep(5);
+  claw_Lower();
+  sleep(1);
+  claw_Grab();
+  bool hold = claw_HoldsBall();
+  printf("Hold value is %d \n", hold);
+  sleep(3);
+  claw_Throw();
+  
+  
+  return 0;
+}
