24125fd27b15e6802ae231745c51a3609208c9df
Trym Sneltvedt <trysne@gmail.com>
2018-12-10 17:06:28 +0100

    Fixes script files for cross compilation with docker

--- src/kobe.sh
+++ src/kobe.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
-if [ $1 = "crosscompile" ]; then
-    echo "Cross compiling..."
-    arm-linux-gnueabi-gcc -std=c11 -Wall -O2 \
-        obj/*.o \
-        adapters/detect.c \
-        drive.c \
-        tests/scan_test.c \
-        -o scan_test
-    echo "Done."
-fi
-
+case $1 in
+    "tests")
+        echo "Compiling..."
+        arm-linux-gnueabi-gcc \
+            -std=c11 \
+            -Wall \
+            -O2 \
+            -D _BSD_SOURCE \
+            obj/*.o \
+            adapters/detect.c \
+            drive.c \
+            tests/scan_test.c \
+            -o bin/scan_test \
+            && echo "Compiling done."
+        ;;
+    *)
+        echo "No args given, please feed me args."
+esac
 
