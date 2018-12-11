5a24ec7c6b8dce08c6f6d937298d8cf617e052ed
Trym Sneltvedt <trysne@gmail.com>
2018-12-11 14:09:05 +0100

    Removes unnecessary part of crossmake script and removes Dockerfile

--- crossmake.sh
+++ crossmake.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "\nKOB-E CROSS COMPILER\n"
 echo "Starting docker container..."
-docker run --name kob-e -v $(pwd):/home/compiler -w /home/compiler \
+docker run --name kob-e -v $(pwd):/home/compiler \
     ev3dev/debian-jessie-cross \
     make $1
 echo "Docker container finished, cleaning up..."
