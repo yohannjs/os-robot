dc2896771feb533103e2e512c0cc1bb696725363
Kob-e squad <sneltved@eurecom.fr>
2018-12-14 15:31:30 +0000

    Finished claw functions (for now).

--- src/interfaces/claw.h
+++ src/interfaces/claw.h
@@ -5,7 +5,8 @@ int claw_Init();
 int claw_Lower();
 int claw_Grab();
 int claw_Throw();
-bool claw_HoldsBall();
+int claw_HoldsBall();
 int claw_Drop();
+int claw_Reset();
 
 #endif
--- tests/claw_test.c
+++ tests/claw_test.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdbool.h>
 
 #include "claw.h"
 
@@ -9,21 +8,14 @@ int main(){
   {
     printf("Initialized claw \n");
   } else return -1;
-  sleep(1);
-  claw_Throw();
-  sleep(1);
-  /*sleep(5);
-  get_tacho_position(big_motor, &big_motor_pos);
-  printf("Position: %d \n",big_motor_pos); */
-  //sleep(5);
-  claw_Lower();
-  sleep(1);
-  claw_Grab();
-  bool hold = claw_HoldsBall();
-  printf("Hold value is %d \n", hold);
-  sleep(3);
-  claw_Throw();
-  
-  
+  /*
+  if (claw_TakeBall()==1)
+  {
+    printf("Ball caught \n");
+    claw_Drop();
+  }
+  */
+  claw_Drop();
+  claw_Reset();
   return 0;
 }
