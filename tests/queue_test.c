07815b90885ee3b0daadbf0b5bbeb4cdc94bc596
Trym Sneltvedt <trysne@gmail.com>
2018-12-14 22:09:04 +0100

    Adds queue module and accompanying test

--- tests/queue_test.c
+++ tests/queue_test.c
@@ -5,18 +5,95 @@
 
 int main(void)
 {
-    mqd_t q1r;
-    mqd_t q1w;
+    mqd_t q1;
+    mqd_t q2;
     
-    queue_CreateReadDescriptor("/q1", &q1r);
-    queue_CreateWriteDescriptor("/q1", &q1w);
+    printf("Creating queue /q1\n");
+    if (queue_Create("/q1", &q1)) 
+    {
+        printf("\e[91mFailed\e[0m\n");
+    }
+    else
+    {
+        printf("\e[92mSuccess\e[0m\n");
+    }
     
-    queue_Write(q1w, 4);
-
+    printf("Creating queue /q2\n");
+    if (queue_Create("/q2", &q2))
+    {
+        printf("\e[91mFailed\e[0m\n");
+    }
+    else
+    {
+        printf("\e[92mSuccess\e[0m\n");
+    }
+    
+    printf("Putting 4 into queue 1\n");
+    if (queue_Write(q1, 4))
+    {
+        printf("\e[91mFailed\e[0m\n");
+    }
+    else
+    {
+        printf("\e[92mSuccess\e[0m\n");
+    }
+    
+    printf("Reading from queue 1\n");
     int data; 
-    queue_Read(q1r, &data);
+    if (queue_Read(q1, &data)) 
+    {
+        printf("\e[91mFailed\e[0m\n");
+    }
+    else
+    {
+        printf("\e[92mReads %d from queue 1\e[0m\n", data);
+    }
 
-    printf("I received %d\n", data);
+    printf("Trying to read from queue 2, which is empty\n");
+    if (queue_Read(q2, NULL))
+    {
+        printf("\e[92mNothing to read\e[0m\n");    
+    }
+    else
+    {
+        printf("\e[91mSomething was read\e[0m\n");
+    }
+
+    printf("Puts the data from queue 1 multiplied by 3 into queue 2\n");
+    if (queue_Write(q2, data * 3)) 
+    {
+        printf("\e[91mFailed\e[0m\n");
+    }
+    else
+    {
+        printf("\e[92mSuccess\e[0m\n");
+    }
+    
+    printf("Reading from queue 2\n");
+    int data2;
+    if (queue_Read(q2, &data2))
+    {
+        printf("\e[91mFailed\e[0m\n");
+    }
+    else
+    {
+        printf("\e[92mReads %d from queue 2\e[0m\n", data2);
+    }
+
+    printf("Tries to print more messages to the queue than it has room for\n");
+    for (int i=0; i<10; i++) {
+        queue_Write(q1, 1);
+    }
+    if (queue_Write(q1, 1)) 
+    {
+        printf("\e[92mFailed\e[0m\n");
+    }
+    else
+    {
+        printf("\e[91mSuccess\e[0m\n");
+    }
+
+    printf("Done.\n");
 
     return 0;
 }
