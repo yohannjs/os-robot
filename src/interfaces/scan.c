4c8dc950ac6485bcdf1df0f40599a399728f3fca
Yohann Jacob Sandvik <yohannjsandvik@gmail.com>
2019-01-15 15:23:59 +0100

    Add tests for scan_FindBall2()

--- src/interfaces/scan.c
+++ src/interfaces/scan.c
@@ -172,8 +172,7 @@ int scan_FindBallHeading(int NUM_SAMPLES, int* head, int* dist){
 
 int scan_FindBall2(int* samples, int threshold, int *head, int *dist)
 {
-  int min_length = 20;
-  
+  int min_length = 20; 
   int start_heading;
   int end_heading = 0;
   for (int i=0; i<360; i++)
--- tests/scan2_test.c
+++ tests/scan2_test.c
@@ -5,52 +5,44 @@
 #include "drive.h"
 #include "detect.h"
 #include "scan.h"
-#include "navigate.h"
+// #include "navigate.h"
+
 
 int main(int argc, char **argv)
-{    
-		if (argc == 1)
-		{ 
-			int samples[360];
-			int template[360];
-			scan_LoadSamples("samples/samples10.txt", samples);
-			scan_LoadSamples("samples/template01.txt", template);
-			
-			int heading;
-			int distance;
-			printf("samples[0] = %d\n", samples[0]);
-			printf("template[0] = %d\n", template[0]); 
-			scan_FindBall(samples, template, &heading, &distance);
-			
-			printf("Heading: %d\nDistance: %d\n", heading, distance);
-		} 
-		else if (argc == 2) 
-		{
+{  
+    drive_Init();
+    detect_Init(); 
+    int samples[360];
+    int template[360];
+    int heading;
+    int distance;
 
-				drive_Init();
-				detect_Init();
-				int heading;
-				int distance;
-				//navigation_GoToScanPosition(MIDDLE, UP);
-				//sleep(3);
-				int samples[360];
-				int threshold = 200;
-				scan_Scan360( samples, 3 );
-				printf("Done with scan, moving to file \n");
-				printf(argv[1]);
-				scan_FindBall2(samples, threshold, &heading, &distance);
-				scan_WriteSamplesToFile( samples, argv[1] );
-				//navigation_MoveToBall(distance, heading);
-				//int adjustDistance;
-				//detect_GetDistance();
-				//navigation_AdjustBallDistance(adjustDistance);
+    if (argc == 1)
+    { 
+        scan_LoadSamples("samples/samples10.txt", samples);
+        scan_LoadSamples("samples/template01.txt", template);
+        printf("samples[0] = %d\n", samples[0]);
+        printf("template[0] = %d\n", template[0]); 
+        scan_FindBall(samples, template, &heading, &distance);
+        printf("Heading: %d\nDistance: %d\n", heading, distance);
+    } 
+    else if (argc == 2) 
+    {
+        int threshold = 200;
+        scan_Scan360( samples, 3 );
+        printf("Done with scan, moving to file \n");
+        // printf(argv[1]);
+        scan_FindBall2(samples, threshold, &heading, &distance);
+        scan_WriteSamplesToFile( samples, argv[1] );
+        printf("Heading ball: %d \n", heading);
+        printf("Distance ball: %d \n", distance);
 
-		}
-		else
-		{
-			printf("lol nope\n");
-		}
-		return 0;
+    }
+    else
+    {
+        printf("lol nope\n");
+    }
+    return 0;
 }
 
 
