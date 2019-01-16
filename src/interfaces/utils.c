024079af5a6d98cff2c5c376b14d2ac6e7c992a6
Kob-e squad <sneltved@eurecom.fr>
2019-01-16 14:52:47 +0000

    Adds log functions for numbers and conforms to naming convention, fixes battery function

--- src/interfaces/utils.c
+++ src/interfaces/utils.c
@@ -1,28 +1,38 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void utils_sleep(int ms)
+void utils_Sleep(int ms)
 {
     usleep(ms * 1000);
 }
 
-void utils_log(const char *module_name, const char *msg)
+void utils_Log(const char *module_name, const char *msg)
 {
     printf("[%s] %s\n", module_name, msg);
 }
 
-void utils_err(const char *module_name, const char *msg)
+void utils_Err(const char *module_name, const char *msg)
 {
     printf("\e[91m[%s] %s\e[0m\n", module_name, msg);
 }
 
-float utils_battery(void)
+void utils_LogInt(const char *module_name, const char *msg, int number)
+{
+    printf("[%s] %s%d\n", module_name, msg, number);
+}
+
+void utils_ErrInt(const char *module_name, const char *msg, int number)
+{
+    printf("\e[91m[%s] %s%d\e[0m\n", module_name, msg, number);
+}
+
+int utils_Battery(void)
 {
     FILE *f;
-    f = fopen("/sys/class/power_supply/legoev3/battery_now", "r");
+    f = fopen("/sys/class/power_supply/legoev3-battery/voltage_now", "r");
     int battery_voltage;
     fscanf(f, "%d", &battery_voltage);
     fclose(f);
 
-    return ((float) battery_voltage) / 1000;
+    return battery_voltage / 10000;
 }
--- src/interfaces/utils.h
+++ src/interfaces/utils.h
@@ -4,23 +4,34 @@
 /**
  * Makes the current thread wait for a specified number of milliseconds.
  */
-void utils_sleep(int ms);
+void utils_Sleep(int ms);
 
 /**
  * Prints a message on a standardized format with the name of the calling
  * module.
  */
-void utils_log(const char *module_name, const char *msg);
+void utils_Log(const char *module_name, const char *msg);
 
 /**
- * Same as utils_log but print is bright red.
+ * Same as utils_Log but print is bright red.
  */
-void utils_err(const char *module_name, const char *msg);
+void utils_Err(const char *module_name, const char *msg);
 
 /**
- * Returns the current battery voltage of the EV3 lego brick as a float. Unit
+ * Prints a message with following number (integer) on standardized form with
+ * module name.
+ */
+void utils_LogInt(const char *module_name, const char *msg, int number);
+
+/**
+ * Same as utils_LogInt but in error red
+ */
+void utils_ErrInt(const char *module_name, const char *msg, int number);
+
+/**
+ * Returns the current battery voltage of the EV3 lego brick as an int. Unit
  * is volts.
  */
-float utils_battery(void);
+int utils_Battery(void);
 
 #endif
