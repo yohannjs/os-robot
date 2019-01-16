1bb275b957b25260d5b42d048925dfa8370323e0
Kob-e squad <sneltved@eurecom.fr>
2019-01-16 14:53:28 +0000

    Updates utils_test to test new functions

--- tests/utils_test.c
+++ tests/utils_test.c
@@ -1,14 +1,18 @@
+#include <stdio.h>
+
 #include "utils.h"
 
 static const char* mn = "  UTIL  ";
 
 int main()
 {
-    utils_log(mn, "Ordinary logging works.");
-    utils_err(mn, "Errors too!");
-    utils_log(mn, "Now I'm supposed to wait for 2.5 seconds...");
-    utils_sleep(2500);
-    utils_log(mn, "You will have to judge how well it went.");
-
+    utils_Log(mn, "Ordinary logging works.");
+    utils_Err(mn, "Errors too!");
+    utils_Log(mn, "Now I'm supposed to wait for 2.5 seconds...");
+    utils_Sleep(2500);
+    utils_Log(mn, "You will have to judge how well it went.");
+    int voltage = utils_Battery();
+    utils_LogInt(mn, "Battery voltage: ", voltage);
+    utils_ErrInt(mn, "Error number: ", 0);
     return 0;
 }
